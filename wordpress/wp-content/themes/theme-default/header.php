856f0af4b467fdf9d3b96ebc1e209c4f748165c1
Renan Emigdio <renanemigdiogoncalves@hotmail.com>
2017-02-18 00:40:31 -0200

    v4

--- wordpress/wp-content/themes/theme-default/header.php
+++ wordpress/wp-content/themes/theme-default/header.php
@@ -28,28 +28,30 @@
 
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> onload="myFunction()" style="margin:0;">
 
-<?php if ( is_front_page() ) { ?>
-	
-	<div class="header scrollme animateme" data-when="view" data-from="0.5" data-to="0" data-opacity="0" data-easing="easeout">
-	    <div class="logo">
-	        <a href="<?php echo get_home_url(); ?>">
-	            <img src="<?php echo get_bloginfo('template_directory');?>/img/header_png.png" alt="Le Corbeaux">
-	        </a>
-	    </div>
-	</div>
+<div id="myDiv" class="animate-bottom">
 
-<?php } else { ?>
+	<?php if ( is_front_page() ) { ?>
+		
+		<div class="header scrollme animateme" data-when="view" data-from="0.5" data-to="0" data-opacity="0" data-easing="easeout">
+		    <div class="logo">
+		        <a href="<?php echo get_home_url(); ?>">
+		            <img src="<?php echo get_bloginfo('template_directory');?>/img/header_png.png" alt="Le Corbeaux">
+		        </a>
+		    </div>
+		</div>
 
-	<div class="header">
-	    <div class="logo">
-	        <a href="<?php echo get_home_url(); ?>">
-	            <img src="<?php echo get_bloginfo('template_directory');?>/img/header_png.png" alt="Le Corbeaux">
-	        </a>
-	    </div>
-	</div>
+	<?php } else { ?>
 
-<?php } ?>
+		<div class="header">
+		    <div class="logo">
+		        <a href="<?php echo get_home_url(); ?>">
+		            <img src="<?php echo get_bloginfo('template_directory');?>/img/header_png.png" alt="Le Corbeaux">
+		        </a>
+		    </div>
+		</div>
+
+	<?php } ?>
 
 
--- wordpress/wp-content/themes/theme-default/page-default-1.php
+++ wordpress/wp-content/themes/theme-default/page-default-1.php
@@ -1,8 +1,10 @@
 <?php /* Template Name: Home Page Template */ ?>
 
+<div id="loader" style="display:block;"></div>  
+
 <?php get_header(); ?>
         
-    <div class="container boxhome">
+    <div class="container">
 
         <div class="horizon scroll">
             <img src="<?php echo get_bloginfo('template_directory');?>/img/horizonn.png" alt="" />
@@ -16,7 +18,7 @@
             
             <div class="case-1 page blur">
                 <h2>Hello Friend</h2>
-                <p>Welcome to Le_Corbeaux. </br>Content made to expand your mind. </br> Site in develop.</p>
+                <p>Welcome to Le Corbeaux. </br>Content made to expand your mind. </br> Site in develop.</p>
             </div>
             
             <div class="case-2 page blur">
@@ -24,45 +26,46 @@
                 <div class="entry">
                     <?php
 
-                    global $post;
-                    $args = array( 'posts_per_page' => 5, 'offset'=> 1, 'category' => 'Teste' );
+                        global $post;
+                        $args = array( 'posts_per_page' => 5, 'offset'=> 1, 'category' => 'Teste' );
+                        $myposts = get_posts( $args );
 
-                    $myposts = get_posts( $args );
-                    foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-                        <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                    <?php endforeach; 
+                        foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+
+                            <p>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </p>
+
+                        <?php endforeach; 
+
                     wp_reset_postdata();?>
-
                 </div>
             </div>           
 
             <div class="case-3 page blur">
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</br>
-                    et dolore magna aliqua.</br>
-                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</br>et dolore magna aliqua.</br></p>
             </div>
             
             <div class="case-4 page blur">
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</br>
-                    et dolore magna aliqua.</br>
-                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</br>et dolore magna aliqua.</br></p>
             </div>
 
             <div class="case-5 page blur">
                 <h2>Recent Posts</h2>
                 <div class="entry">
-                <?php
-                    $args = array( 'numberposts' => '7' );
-                    $recent_posts = wp_get_recent_posts( $args );
-                    foreach( $recent_posts as $recent ){
-                        echo '<p><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></p>';
-                    }
-                    wp_reset_query();
-                ?>
+                
+                    <?php
+
+                        $args = array( 'numberposts' => '7' );
+                        $recent_posts = wp_get_recent_posts( $args );
+                        foreach( $recent_posts as $recent ) {
+                            echo '<p><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></p>';
+                        }
+
+                    wp_reset_query();?>
+
                 </div>
             </div>
 
@@ -88,9 +91,24 @@
 
     <script type="text/javascript">
 
-    (function($) {
-        $.jInvertScroll(['.scroll']);
-    }(jQuery));
+        (function($) {
+            $.jInvertScroll(['.scroll']);
+        }(jQuery));
+    
+        var myVar;
+
+        function myFunction() {
+            $('html').addClass('crow');
+            $('#myDiv').addClass('none');
+            myVar = setTimeout(showPage, 8000);
+        }
+
+        function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+            $('html').removeClass('crow');
+            $('#myDiv').removeClass('none');
+        }
 
     </script>
 
